e7b54300de841addfdc9486dd8c30c7a91d11961
Leonard Li <ypling@Zhiyuans-MacBook-Pro.local>
2014-10-13 10:44:48 -0700

    Initial Commit

---
+++ StripeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StripeTest
+//
+//  Created by Leonard Li on 10/13/14.
+//  Copyright (c) 2014 Leonard Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StripeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StripeTest
+//
+//  Created by Leonard Li on 10/13/14.
+//  Copyright (c) 2014 Leonard Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
